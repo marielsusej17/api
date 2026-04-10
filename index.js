1fe6fffce84eb1d1b49eb5f2d9aef72ca0a95448
marielsusej17 <17mariel122007@gmail.com>
2026-04-10 10:17:44 -0500

    fix render error

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-require('dotenv').config();
-const Server = require('./modules/server');
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import peliculasRoutes from "./src/routes/peliculas.routes.js";
 
-const server = new Server();
-server.listen();
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", peliculasRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("🟢 Conectado a MongoDB"))
+  .catch(err => console.error("Error de conexión:", err));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("🚀 Servidor en puerto " + process.env.PORT);
+});
